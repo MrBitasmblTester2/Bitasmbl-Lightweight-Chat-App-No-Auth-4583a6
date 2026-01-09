40c09bc20fd8f161100867a0e7fa5d084b85b413
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 13:24:27 +0400

    Add ChatRoom.vue

---
+++ frontend/src/components/ChatRoom.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <form @submit.prevent="join">
+      <input v-model="nickname" placeholder="Nickname" />
+      <input v-model="room" placeholder="Room" />
+      <button type="submit">Join</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import socket from '../socket'
+export default { name:'ChatRoom', data(){return{nickname:'',room:''}} }
+</script>
